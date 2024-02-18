03d94f3e196985a081472fc042fb1da7181e4bd0
Noman Shamim <nomanshamim2004@gmail.com>
2024-02-18 17:25:40 +0500

    Woring on Home page

--- src/AllBlogs/Blogs.jsx
+++ src/AllBlogs/Blogs.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Blogs.css'
+import user from '../assets/user.png'
 
 export default function Blogs() {
   return (
     <div className='main'>
-        <h1 id='welcome'><span>W</span>elCome All Members!</h1>
-        <h3 id='ABlogs'>All Blogs</h3>
-        <div className='blogDiv'>
-
-        </div>
+      <h1 id='welcome'><span>W</span>elCome All Members!</h1>
+      <h3 id='ABlogs'>All Blogs</h3>
+      <div className='blogDiv'>
+        <img src={user} alt="" id='userproimg' />
+        <h4 id='userproHead'>Artificial intelligent</h4>
+        <h6 id='userpronames'>Muhammad Noman - <span>{new Date().toLocaleDateString()}</span></h6>
+      </div>
     </div>
-    
+
   )
 }
