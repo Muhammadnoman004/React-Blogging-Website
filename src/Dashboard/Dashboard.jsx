5a07118b204ad3cf6d8e8fbb3bf8714d63cf4a3f
Noman Shamim <nomanshamim2004@gmail.com>
2024-02-19 23:42:26 +0500

    Working on User Dashboard page

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './Dashboard.css'
+import logo from '../assets/blog-removebg-preview.png'
 
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard...</h1>
+      <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container-fluid" id='nav'>
+          <img src={logo} alt="" id='logo' />
+          <a className="navbar-brand navtext" href="#">MyBlog</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item homenav">
+                <a className="nav-link active" aria-current="page" href="#"></a>
+              </li>
+              <button className='btn fullName'>Full Name</button>
+              <button className='btn home'>Home</button>
+              <button className='btn btn-primary logout'>Logout</button>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+      <br /><br /><br /><br />
+      <h1 id='dash'><span>D</span>ashboard</h1>
     </div>
   )
 }
