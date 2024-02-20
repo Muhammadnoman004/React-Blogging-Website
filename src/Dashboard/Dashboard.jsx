d57ae0a934672b8794c25136e30d56890e43576d
Noman Shamim <nomanshamim2004@gmail.com>
2024-02-20 23:22:06 +0500

    Dashboard UI done

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -27,6 +27,27 @@ export default function Dashboard() {
       </nav>
       <br /><br /><br /><br />
       <h1 id='dash'><span>D</span>ashboard</h1>
+
+      <div className='BlogTitleDiv'>
+
+        <div class="mb-3">
+          <label for="formGroupExampleInput" class="form-label" id='blogtitle'>Blog Title:</label>
+          <input type="text" className="form-control" id="formGroupExampleInput" required placeholder="Enter Blog Title" />
+        </div>
+
+        <div class="mb-3">
+          <label for="validationTextarea" class="form-label" id='blogdescription'>Blog Description:</label>
+          <textarea class="form-control" id="validationTextarea" placeholder="Enter Blog Description" required></textarea>
+        </div>
+        <button className='btn btn-primary publishBtn'>Publish Blog</button>
+      </div>
+
+      <div>
+        <h2 id='ABlogs'>All Blogs</h2>
+        <div className='anyBlogDiv'>
+          <h1>You don't have any blog yet!</h1>
+        </div>
+      </div>
     </div>
   )
 }
